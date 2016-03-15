a71cd5e0f1ea77f1efe042c64cdb0c356a93df7a
Ben Allred <skuttleman@gmail.com>
2016-03-15 17:58:56 -0600

    starting to look like a web page

--- public/js/angular/directives.js
+++ public/js/angular/directives.js
@@ -25,9 +25,18 @@ angular.module('capstone')
       $scope.active = [], $scope.invitations = [];
       $scope.getActive = getGames(Ajax, $scope, 'active');
       $scope.getInvitations = getGames(Ajax, $scope, 'invitations');
+      $scope.getPending = getGames(Ajax, $scope, 'pending');
+      $scope.whoseTurn = function(game) {
+        console.log(game);
+        var user = JSON.parse(localStorage.user);
+        return (user.id == game.player1_id && game.game_status == 'player1 turn') ||
+          (user.id == game.player2_id && game.game_status == 'player2 turn');
+      };
 
       $scope.getActive();
       $scope.getInvitations();
+      $scope.getPending();
+
       Socket.on('invitation update', function(data) {
         // do the thing that makes the app go
         console.log(data);
@@ -44,7 +53,14 @@ function getGames(Ajax, $scope, property) {
   return function() {
     Ajax.get(window.SERVER_HOST + '/api/v1/games/' + property).then(function(results) {
       if ($scope) $scope[property] = results.data.games;
-      console.log(results.data.games);
+      if (property === 'active') {
+        $scope[property].forEach(function(game){
+          var user = JSON.parse(localStorage.user);
+          game.playerTurn = game.game_status == 'player1 turn' ? game.player1 : game.player2;
+          game.otherPlayer = game.player1.id == user.id ? game.player2 : game.player1;
+          game.myTurn = user.id == game.playerTurn.id;
+        });
+      }
       return results.data.games;
     });
   };
