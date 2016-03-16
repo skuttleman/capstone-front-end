0ae05a631f7c21b78fe5e57dcf1440a3da51d033
Ben Allred <skuttleman@gmail.com>
2016-03-16 05:36:29 -0600

    more styling and layout

--- public/js/angular/directives.js
+++ public/js/angular/directives.js
@@ -9,7 +9,7 @@ angular.module('capstone')
     controller: ['$scope', function($scope) {
       $scope.navs = [
         { href: "/#/games/new", text: "New Game" },
-        { href: "/#/settings", text: "Settings" },
+        // { href: "/#/settings", text: "Settings" },
         { href: "/#/logout", text: "Log Out" }
       ];
     }]
@@ -26,22 +26,21 @@ angular.module('capstone')
       $scope.getActive = getGames(Ajax, $scope, 'active');
       $scope.getInvitations = getGames(Ajax, $scope, 'invitations');
       $scope.getPending = getGames(Ajax, $scope, 'pending');
-      $scope.whoseTurn = function(game) {
-        console.log(game);
-        var user = JSON.parse(localStorage.user);
-        return (user.id == game.player1_id && game.game_status == 'player1 turn') ||
-          (user.id == game.player2_id && game.game_status == 'player2 turn');
-      };
 
       $scope.getActive();
       $scope.getInvitations();
       $scope.getPending();
 
-      Socket.on('invitation update', function(data) {
+      Socket.on('accept game', function(data) {
         // do the thing that makes the app go
         console.log(data);
       });
-      Socket.on('active update', function(data) {
+      Socket.on('reject game', function(data) {
+        // do the thing that makes the app go
+        console.log(data);
+      });
+
+      Socket.on('game updated', function(data) {
         // do the thing that makes the app go
         console.log(data);
       });
@@ -53,14 +52,12 @@ function getGames(Ajax, $scope, property) {
   return function() {
     Ajax.get(window.SERVER_HOST + '/api/v1/games/' + property).then(function(results) {
       if ($scope) $scope[property] = results.data.games;
-      if (property === 'active') {
-        $scope[property].forEach(function(game){
-          var user = JSON.parse(localStorage.user);
-          game.playerTurn = game.game_status == 'player1 turn' ? game.player1 : game.player2;
-          game.otherPlayer = game.player1.id == user.id ? game.player2 : game.player1;
-          game.myTurn = user.id == game.playerTurn.id;
-        });
-      }
+      $scope[property].forEach(function(game){
+        var user = JSON.parse(localStorage.user);
+        game.playerTurn = game.game_status == 'player1 turn' ? game.player1 : game.player2;
+        game.otherPlayer = game.player1.id == user.id ? game.player2 : game.player1;
+        game.myTurn = user.id == game.playerTurn.id;
+      });
       return results.data.games;
     });
   };
